10cd90c42d0fd84cf54a3095828a3185ddd04a68
April Howard <ahoward@zipcodes-MacBook-Pro-5.local>
2020-03-02 11:30:05 -0500

    some tests pass and some fail. Will come back to it

--- src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
+++ src/main/java/rocks/zipcode/assessment2/collections/Inventory.java
@@ -1,5 +1,6 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,29 +10,31 @@ public class Inventory {
     /**
      * @param strings list of strings to add / remove / fetch from
      */
-    public Inventory(List<String> strings) {
 
+    List<String> inventory;
+    public Inventory(List<String> strings) {
+        inventory = strings;
     }
 
     /**
      * nullary constructor initializes a new list
      */
     public Inventory() {
-
+       inventory = new ArrayList<>();
     }
 
     /**
      * @param item - increment the number of this item in stock by 1
      */
     public void addItemToInventory(String item) {
-        return;
+        inventory.add(item);
     }
 
     /**
      * @param item - decrement the number of this item in stock by 1
      */
     public void removeItemFromInventory(String item) {
-        return;
+        inventory.remove(item);
     }
 
     /**
@@ -39,6 +42,12 @@ public class Inventory {
      * @return - return the number of items
      */
     public Integer getItemQuantity(String item) {
-        return null;
+        int quantity = 0;
+        for(String s : inventory) {
+            if(s == item) {
+                quantity++;
+            }
+        }
+        return quantity;
     }
 }
--- src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
+++ src/main/java/rocks/zipcode/assessment2/collections/MonthConversion.java
@@ -1,5 +1,8 @@
 package rocks.zipcode.assessment2.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Use a map to solve
  */
@@ -8,32 +11,53 @@ public class MonthConversion {
      * @param monthNumber - ordinal of month in the year; i.e. January = 1, February = 2
      * @param monthName - name of month
      */
-    public void add(Integer monthNumber, String monthName) {
+    Map<Integer, String> months;
 
+    public MonthConversion() {
+        months = new HashMap<>();
     }
+    public void add(Integer monthNumber, String monthName) {
+    months.put(monthNumber, monthName);
+    }
+
 
     /**
      * @param monthNumber - ordinal of month in the year
      * @return the name of the respective month
      */
     public String getName(Integer monthNumber) {
-        throw new NullPointerException();
-    }
+
+        if(months.containsKey(monthNumber)) {
+            return months.get(monthNumber);
+        }
+        else {
+            throw new NullPointerException();
+        }
+
+        }
+
 
     /**
      * @param monthName - name of month
      * @return - the ordinal of the month in the year
      */
     public int getNumber(String monthName) {
+        for (Map.Entry<Integer, String> entry : months.entrySet()) {
+            if (entry.getValue().contains(monthName)) {
+                return entry.getKey();
+            }
+        }
+
         return (Integer)null;
     }
+
 
     /**
      * @param monthNumber
      * @return true if the monthNumber is in the keySet
      */
     public Boolean isValidNumber(Integer monthNumber) {
-        return null;
+        return months.containsKey((monthNumber));
     }
 
     /**
@@ -41,14 +65,14 @@ public class MonthConversion {
      * @return true if the monthName is in the valueSet
      */
     public Boolean isValidMonth(String monthName) {
-        return null;
+        return months.containsKey(monthName);
     }
 
     /**
      * @return number of entries in this mapping
      */
     public Integer size() {
-        return -1;
+        return months.size();
     }
 
     /**
@@ -56,6 +80,6 @@ public class MonthConversion {
      * @param monthName - name of month
      */
     public void update(Integer monthNumber, String monthName) {
-
+    months.replace(monthNumber, monthName);
     }
 }
